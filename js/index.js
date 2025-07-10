a50ca4324879bc3fe95277650fb018ea50b9fa76
Dasha <darina1970@mail.ru>
2025-07-10 18:52:11 +0700

    feat: add adaptive styles to the sections

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import { setupBurgerMenu } from "./burgerMenu.js";
 import { animateOnScroll } from "./animateOnScroll.js";
+import { startMatrixEffect } from "./startMatrixEffect.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     setupBurgerMenu();
     animateOnScroll();
+    startMatrixEffect();
 
 })
---
+++ js/startMatrixEffect.js
@@ -0,0 +1,42 @@
+export function startMatrixEffect() {
+    const canvas = document.getElementById('matrix-canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth;
+    canvas.height = document.querySelector('.code').offsetHeight;
+
+    const letters = '101010101010101010101010101010';
+    const fontSize = 16;
+    const columns = canvas.width / fontSize;
+    const drops = Array.from({ length: columns }, () => 1);
+
+    function draw() {
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.1)'; // fade
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = '#e281a5'; // розовый
+        ctx.font = `${fontSize}px monospace`;
+
+        for (let i = 0; i < drops.length; i++) {
+        const text = letters[Math.floor(Math.random() * letters.length)];
+        const x = i * fontSize;
+        const y = drops[i] * fontSize;
+
+        ctx.fillText(text, x, y);
+
+        if (y > canvas.height && Math.random() > 0.975) {
+            drops[i] = 0;
+        }
+
+        drops[i]++;
+        }
+    }
+
+    setInterval(draw, 50);
+
+  // Обновление размеров при ресайзе
+    window.addEventListener('resize', () => {
+        canvas.width = window.innerWidth;
+        canvas.height = document.querySelector('.code-section').offsetHeight;
+    });
+}
